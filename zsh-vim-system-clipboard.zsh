1d867a13dc0d74080672d18022f7a6d4531cfbeb
xPMo <PMo_@vivaldi.net>
2019-03-06 00:24:21 -0600

    Adopt zvsc_* namespace
    zvsc_(copy|paste)_handlers (user facing)
    _zvsc* and __zvsc* functions for zle

--- zsh-vim-system-clipboard.zsh
+++ zsh-vim-system-clipboard.zsh
@@ -1,36 +1,36 @@
 # {{{ set system clipboard
 zmodload zsh/parameter
-declare -A _system_paste_handlers
-declare -A _system_copy_handlers
+declare -A zvsc_paste_handlers
+declare -A zvsc_copy_handlers
 # "(e)*" to remove special meaning of "*"
 if (( $+commands[termux-clipboard-get] )); then
-	_system_paste_handlers[(e)*]="${_system_paste_handlers[(e)*]:-termux-clipboard-get}"
-	_system_paste_handlers[+]="${_system_paste_handlers[+]:-termux-clipboard-get}"
-	_system_copy_handlers[(e)*]="${_system_copy_handlers[(e)*]:-termux-clipboard-set}"
-	_system_copy_handlers[+]="${_system_copy_handlers[+]:-termux-clipboard-set}"
+	zvsc_paste_handlers[(e)*]="${zvsc_paste_handlers[(e)*]:-termux-clipboard-get}"
+	zvsc_paste_handlers[+]="${zvsc_paste_handlers[+]:-termux-clipboard-get}"
+	zvsc_copy_handlers[(e)*]="${zvsc_copy_handlers[(e)*]:-termux-clipboard-set}"
+	zvsc_copy_handlers[+]="${zvsc_copy_handlers[+]:-termux-clipboard-set}"
 elif (( $+WAYLAND_DISPLAY & $+commands[wl-paste] )); then
-	_system_paste_handlers[(e)*]="${_system_paste_handlers[(e)*]:-wl-paste -p -n}"
-	_system_paste_handlers[+]="${_system_paste_handlers[+]:-wl-paste -n}"
-	_system_copy_handlers[(e)*]="${_system_copy_handlers[(e)*]:-wl-copy -p}"
-	_system_copy_handlers[+]="${_system_copy_handlers[+]:-wl-copy}"
+	zvsc_paste_handlers[(e)*]="${zvsc_paste_handlers[(e)*]:-wl-paste -p -n}"
+	zvsc_paste_handlers[+]="${zvsc_paste_handlers[+]:-wl-paste -n}"
+	zvsc_copy_handlers[(e)*]="${zvsc_copy_handlers[(e)*]:-wl-copy -p}"
+	zvsc_copy_handlers[+]="${zvsc_copy_handlers[+]:-wl-copy}"
 elif (( $+DISPLAY & $+commands[xclip] )); then
-	_system_paste_handlers[(e)*]="${_system_paste_handlers[(e)*]:-xclip -out}"
-	_system_paste_handlers[+]="${_system_paste_handlers[+]:-xclip -selection clipboard -out}"
-	_system_copy_handlers[(e)*]="${_system_copy_handlers[(e)*]:-xclip}"
-	_system_copy_handlers[+]="${_system_copy_handlers[+]:-xclip -selection clipboard}"
+	zvsc_paste_handlers[(e)*]="${zvsc_paste_handlers[(e)*]:-xclip -out}"
+	zvsc_paste_handlers[+]="${zvsc_paste_handlers[+]:-xclip -selection clipboard -out}"
+	zvsc_copy_handlers[(e)*]="${zvsc_copy_handlers[(e)*]:-xclip}"
+	zvsc_copy_handlers[+]="${zvsc_copy_handlers[+]:-xclip -selection clipboard}"
 elif (( $+DISPLAY & $+commands[xsel] )); then
-	_system_paste_handlers[(e)*]="${_system_paste_handlers[(e)*]:-xsel -o}"
-	_system_paste_handlers[+]="${_system_paste_handlers[+]:-xsel -b -o}"
-	_system_copy_handlers[(e)*]="${_system_copy_handlers[(e)*]:-xsel -i}"
-	_system_copy_handlers[+]="${_system_copy_handlers[+]:-xsel -b -i}"
+	zvsc_paste_handlers[(e)*]="${zvsc_paste_handlers[(e)*]:-xsel -o}"
+	zvsc_paste_handlers[+]="${zvsc_paste_handlers[+]:-xsel -b -o}"
+	zvsc_copy_handlers[(e)*]="${zvsc_copy_handlers[(e)*]:-xsel -i}"
+	zvsc_copy_handlers[+]="${zvsc_copy_handlers[+]:-xsel -b -i}"
 fi
-(( ${#_system_paste_handlers} + ${#_system_copy_handlers} )) || return
+(( ${#zvsc_paste_handlers} + ${#zvsc_copy_handlers} )) || return
 # }}}
 # {{{ shadow all yank commands
-__yank-clipboard(){
+__zvsc-yank(){
 	# if no copy handler is registered for the given system register,
 	# then run with the default register
-	if ! (( ${+_system_copy_handlers[$_system_register]} )); then
+	if ! (( ${+zvsc_copy_handlers[$_zvsc_register]} )); then
 		zle "$1"
 		return "$?"
 	fi
@@ -38,58 +38,58 @@ __yank-clipboard(){
 	local x
 	x=$registers[x]
 	zle "$1"
-	eval ${_system_copy_handlers[$_system_register]} <<< "${registers[x]}"
+	eval ${zvsc_copy_handlers[$_zvsc_register]} <<< "${registers[x]}"
 	registers[x]="$x"
-	unset _system_register
+	unset _zvsc_register
 }
 
-vi-delete-clipboard(){ __yank-clipboard .vi-delete }
+_zvsc-vi-delete(){ __zvsc-yank .vi-delete }
 
-vi-delete-char-clipboard(){ __yank-clipboard .vi-delete-char }
+_zvsc-vi-delete-char(){ __zvsc-yank .vi-delete-char }
 
-vi-kill-line-clipboard(){ __yank-clipboard .vi-kill-line }
+_zvsc-vi-kill-line(){ __zvsc-yank .vi-kill-line }
 
-vi-kill-eol-clipboard(){ __yank-clipboard .vi-kill-eol }
+_zvsc-vi-kill-eol(){ __zvsc-yank .vi-kill-eol }
 
-vi-change-clipboard(){ __yank-clipboard .vi-change }
+_zvsc-vi-change(){ __zvsc-yank .vi-change }
 
-vi-change-eol-clipboard(){ __yank-clipboard .vi-change-eol }
+_zvsc-vi-change-eol(){ __zvsc-yank .vi-change-eol }
 
-vi-change-whole-line-clipboard(){ __yank-clipboard .vi-change-whole-line }
+_zvsc-vi-change-whole-line(){ __zvsc-yank .vi-change-whole-line }
 
-vi-yank-clipboard(){ __yank-clipboard .vi-yank }
+_zvsc-vi-yank(){ __zvsc-yank .vi-yank }
 
-vi-yank-whole-line-clipboard(){ __yank-clipboard .vi-yank-whole-line }
+_zvsc-vi-yank-whole-line(){ __zvsc-yank .vi-yank-whole-line }
 
-vi-yank-eol-clipboard(){ __yank-clipboard .vi-yank-eol }
+_zvsc-vi-yank-eol(){ __zvsc-yank .vi-yank-eol }
 # }}}
 # {{{ shadow all put commands
-__paste-clipboard(){
-	if ! (( ${+_system_paste_handlers[$_system_register]} )); then
+__zvsc-paste(){
+	if ! (( ${+zvsc_paste_handlers[$_zvsc_register]} )); then
 		zle "$1"
 		return "$?"
 	fi
 	# word splitting
-	CUTBUFFER="$(eval ${_system_paste_handlers[$_system_register]})"
+	CUTBUFFER="$(eval ${zvsc_paste_handlers[$_zvsc_register]})"
 	zle .vi-set-buffer ''
 	zle "$1"
-	unset _system_register
+	unset _zvsc_register
 }
 
-vi-put-after-clipboard(){ __paste-clipboard .vi-put-after }
+_zvsc-vi-put-after(){ __zvsc-paste .vi-put-after }
 
-vi-put-before-clipboard(){ __paste-clipboard .vi-put-before }
+_zvsc-vi-put-before(){ __zvsc-paste .vi-put-before }
 # }}}
 # {{{ shadow vi-set-buffer
-vi-set-buffer-clipboard(){
+_zvsc-vi-set-buffer(){
 	local v
 	read -k 1 v
 	case $v in
 		''|[a-zA-Z0-9_])
-			unset _system_register
+			unset _zvsc_register
 			zle .vi-set-buffer "$v"
 		;;
-		*) _system_register="$v" ;;
+		*) _zvsc_register="$v" ;;
 	esac
 }
 # }}}
@@ -101,7 +101,7 @@ for w in vi-delete vi-delete-char vi-kill-line vi-kill-eol \
 do
 	# TODO: best practice?
 	# overwrite old widgets
-	zle -N "$w" "${w}-clipboard"
+	zle -N "$w" "_zvsc-${w}"
 done
 # }}}
 # vim:foldmethod=marker
